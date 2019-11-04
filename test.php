e3e3276dbde668d000fb24e3a09257eb68b2148f
Willyh Hariardi <willyhr28@gmail.com>
2019-11-04 10:48:59 +0700

    fix code and tranfer member

--- test.php
+++ test.php
@@ -13,17 +13,45 @@ try {
 }
 
 $file = fopen('last_sync_at.txt', 'r') or die ('unable to open file');
+
 if(filesize('last_sync_at.txt') > 0) {
     $last_sync_at = fread($file, filesize('last_sync_at.txt'));
-    // $date = date('#m/d/Y h:i:s a#');
-    $sql  = "SELECT * FROM acc_monitor_log where acc_monitor_log.time > $last_sync_at";
+    echo $last_sync_at;
+    $sqlUM  = "SELECT * FROM USERINFO where USERINFO.HIREDDAY < $last_sync_at";
 }
 
-$rs = odbc_exec($db, $sql);
+echo $sqlUM;
+$rsUM = odbc_exec($db, $sqlUM);
+$tz = 'Asia/Jakarta';
+$timestamp = time();
+$dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
+$dt->setTimestamp($timestamp); 
 
-while($arr = odbc_fetch_array($rs)) {
-    print_r($arr);
-    echo "\n";
+
+$conn = mysqli_connect("localhost","root","","muscle_lab");
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-fclose($file);
+$arrSqlUserMachine = array();
+while($arrUM = odbc_fetch_array($rsUM)) {
+    // print_r($arr);
+    $arrSqlUserMachine[] = '('.$arrUM['Badgenumber'].', "'.$arrUM['acc_enddate'].'", "'.$dt->format('Y-m-d H:i:s').'", "'.$dt->format('Y-m-d H:i:s').'")';
+}
+
+$truncatetable = "TRUNCATE TABLE user_machines";
+if ($conn->query($truncatetable) === TRUE) {
+    echo "Delete all successfully";
+}
+
+if (count($arrSqlUserMachine) > 0) {
+    $msqlQueryUserMachine = 'INSERT INTO user_machines (member_id, expired_date, created_at, updated_at) VALUES '.implode(',', $arrSqlUserMachine);
+    
+    if ($conn->query($msqlQueryUserMachine) === TRUE) {
+        echo "New record User machine created successfully";
+    } else {
+        echo "Error: " . $msqlQueryUserMachine . "<br>" . $conn->error;
+    }$conn->close();
+}
--- uploader.php
+++ uploader.php
@@ -22,7 +22,7 @@ if(filesize('last_sync_at.txt') > 0) {
 }
 
 $rs = odbc_exec($db, $sql);
-if(odbc_num_rows($rs)) {
+if(!odbc_num_rows($rs)) {
     $fp = fopen('last_sync_at.txt', 'w');
     $date = date('#m/d/Y h:i:s a#');
     fwrite($fp, $date);
@@ -30,29 +30,76 @@ if(odbc_num_rows($rs)) {
     echo "<script>window.close();</script>";
     die('no data') ;
 }
+
 $arrSql = array();
 while($arr = odbc_fetch_array($rs)) {
-    $status = $arr['event_point_id'] == 2 ? 'in' : 'out';
-    $arrSql[] = '('.$arr['pin'].', "'.$status.'", "'.$arr['time'].'", "'.$dt->format('Y-m-d H:i:s').'", "'.$dt->format('Y-m-d H:i:s').'")';
+    if($arr['pin'] != 0 && $arr['event_type'] != 1029) {
+        $status = $arr['event_point_id'] == 2 ? 'in' : 'out';
+        $arrSql[] = '('.$arr['pin'].', "'.$status.'", "'.$arr['time'].'", "'.$dt->format('Y-m-d H:i:s').'", "'.$dt->format('Y-m-d H:i:s').'")';
+    }
+    
 }
 
-$conn = mysqli_connect("localhost","root","","muscle_lab");
+$conn = mysqli_connect("us-cdbr-iron-east-05.cleardb.net","bb27a67a215d0a","c7297fbc","heroku_8d8925842adf521");
 // Check connection
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$msqlQuery = 'INSERT INTO attendances  (member_id, status, log_date, created_at, updated_at) VALUES '.implode(',', $arrSql);
-if ($conn->query($msqlQuery) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $msqlQuery . "<br>" . $conn->error;
-}$conn->close();
 
-$fp = fopen('last_sync_at.txt', 'w');
-$date = date('#m/d/Y h:i:s a#');
-fwrite($fp, $date);
-fclose($file);
+try {
+    $db = odbc_connect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=$database_path", "admin", "");
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+// echo $last_sync_at;
+if($last_sync_at) {
+    $sqlUM  = "SELECT * FROM USERINFO";
+}
+
+// echo $sqlUM;
+$rsUM = odbc_exec($db, $sqlUM);
+
+$arrSqlUserMachine = array();
+while($arrUM = odbc_fetch_array($rsUM)) {
+    // print_r($arr);
+    $arrSqlUserMachine[] = '('.$arrUM['Badgenumber'].', "'.$arrUM['acc_enddate'].'", "'.$dt->format('Y-m-d H:i:s').'", "'.$dt->format('Y-m-d H:i:s').'")';
+}
+
+$truncatetable = "TRUNCATE TABLE user_machines";
+if ($conn->query($truncatetable) === TRUE) {
+    echo "Delete all successfully";
+}
+if (count($arrSqlUserMachine) > 0) {
+    $msqlQueryUserMachine = 'INSERT INTO user_machines (member_id, expired_date, created_at, updated_at) VALUES '.implode(',', $arrSqlUserMachine);
+    
+    if ($conn->query($msqlQueryUserMachine) === TRUE) {
+        echo "New record User machine created successfully";
+    } else {
+        echo "Error: " . $msqlQueryUserMachine . "<br>" . $conn->error;
+    }
+}
+
+
+if (count($arrSql) > 0) {
+    $msqlQuery = 'INSERT INTO attendances (member_id, status, log_date, created_at, updated_at) VALUES '.implode(',', $arrSql);
+    if ($conn->query($msqlQuery) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $msqlQuery . "<br>" . $conn->error;
+    }$conn->close();
+}
+
+if (count($arrSql) > 0 || count($arrSqlUserMachine) > 0) {
+    $fp = fopen('last_sync_at.txt', 'w');
+    $timezone  = 7;
+    $date = gmdate("#m/d/Y h:i:s a#", time() + 3600*($timezone+date("I")));
+    fwrite($fp, $date);
+    fclose($file);
+} else {
+    echo "No data";
+}
 
 echo "<script>window.close();</script>";
 ?>
